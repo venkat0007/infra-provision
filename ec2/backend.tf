470df82daa35e9f9250996d09ba5961147933f11
venkatareddy <vvreddy1251@gmail.com>
2024-02-21 11:08:57 +0530

    adding s3 backend

---
+++ ec2/backend.tf
@@ -0,0 +1,10 @@
+$ cat backend.tf
+terraform {
+  backend "s3" {
+    bucket         = "terraform-statefile-infra"
+    key            = "infrastatefile.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform_state" // Optional for state locking
+  }
+}
+
