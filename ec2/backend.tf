73d2228003f311a99db63fba81b76136538bf540
venkatareddy <vvreddy1251@gmail.com>
2024-02-21 11:07:10 +0530

    removed backend

--- ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-statefile-infra"
-    key            = "infrastatefile.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "terraform_state" // Optional for state locking
-  }
-}
-
