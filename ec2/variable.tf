a9d66d1b08cf3a960d45063b0dff64208c0c9ac8
venkatareddy <vvreddy1251@gmail.com>
2024-02-21 10:10:18 +0530

    adding code for ec2

---
+++ ec2/variable.tf
@@ -0,0 +1,14 @@
+variable "access_key" {
+ type = string
+ default = "AKIAZ6BYF23TBLB4654K"
+ }
+
+variable "secret_key"{
+type =string
+ default = "Wzbqo6J9N702yPTl+vON6keeLplFSLAQn2QTToW1"
+ }
+ 
+ variable "ami"{
+  type = string
+  default = "ami-09694bfab577e90b0"
+  }
