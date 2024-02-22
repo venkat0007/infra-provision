2b62106668ff93cbe1d72bbd8ef46b4dc9c181d5
venkatareddy <vvreddy1251@gmail.com>
2024-02-22 09:44:34 +0530

    updated files

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,8 +1,8 @@
-#terraform {
- # backend "s3" {
-  #  bucket         = "infra1-terraform-state-backend"
-   # key            = "terraform.tfstate"
-    #region         = "us-east-2"
-    #dynamodb_table = "terraform_state"
- # }
-#}
+terraform {
+   backend "s3" {
+     bucket         = "infra1-terraform-state-backend"
+     key            = "terraform.tfstate"
+     region         = "us-east-2"
+     dynamodb_table = "terraform_state"
+  }
+}
--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
-#resource "aws_instance" "ec2_instance" {
- #   ami = var.ami
-  #  count = 1
-   # instance_type = "t2.micro"
-    #key_name = "windows"
-#} 
+resource "aws_instance" "ec2_instance" {
+     ami = var.ami
+     count = 1
+     instance_type = "t2.micro"
+     key_name = "windows"
+} 
